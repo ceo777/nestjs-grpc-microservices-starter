f710c22685a8b008e888e7b697e90cfa061abcb5
Oleg Dubnov <61102565+ceo777@users.noreply.github.com>
2024-01-19 09:25:38 +0300

    feat(gateway): use Fastify as HTTP provider
    Install and use Fastify platform as HTTP server provider.

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -6,15 +6,24 @@
  */
 
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const HOST = process.env.HOST || 'localhost';
   const PORT = process.env.PORT || 3000;
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+      AppModule,
+      new FastifyAdapter()
+  );
+
   await app.listen(PORT, HOST).then(() => {
     console.log(`The API Gateway is listening on: ${HOST}:${PORT}`);
   });
 }
+
 bootstrap();
